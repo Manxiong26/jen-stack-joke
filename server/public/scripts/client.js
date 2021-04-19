a15e34ff5f2df1aa02d17201c8e07c687a9bbb74
Maneena Xiong <xiong.maneena@gmail.com>
2021-04-19 11:08:25 -0500

    created newJokes function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,3 +5,18 @@ $( document ).ready( onReady );
 function onReady() {
     console.log('DOM ready');
 }
+
+function addJokes(){
+    let newJokes ={
+        whoseJoke: $(`#whoseJokeIn`).val(),
+        jokeQuestion: $(`#questionIn`).val(),
+        punchLine: $(`#punchlineIn`).val()
+    }
+    console.log('Adding Jokes', newJokes);
+    
+    $.ajax({
+        method: "POST",
+        url: '/jokes',
+        data: newJokes
+    })
+}
